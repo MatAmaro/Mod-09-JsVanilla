a5849eb4751e58bb68e21a353593dd1fa638b9f9
Matheus Amaro <mat.amaro427@gmail.com>
2022-11-11 19:39:29 -0300

    Adição do layout da tabela no js

--- tabela/table.js
+++ tabela/table.js
@@ -1,18 +1,18 @@
-const main  = document.querySelector("main");
+const main = document.querySelector("main");
 let table = document.createElement('table');
-let headers = ['Nome','Espécie','País', 'Quantidade'];
+let headers = ['Nome', 'Espécie', 'País', 'Quantidade'];
 
 let animals = [
-    {nome:'Onça-pintada', especie: 'Panthera onca', pais: 'Brasil', quantidade: 10000},
-    {nome:'Lobo-guará', especie: 'Chrysocyon brachyurus', pais: 'Brasil', quantidade: 50},
-    {nome:'Panda-gigante', especie: 'Ailuropoda melanoleuca', pais: 'China', quantidade: 2500},
-    {nome:'Baleia-fin', especie: 'Balaenoptera physalus', pais: 'Antártida', quantidade: 2000},
-    {nome:'Arara-azul-de-lear', especie: 'Anodorhynchus leari', pais: 'Brasil', quantidade: 1200},
-    {nome:'Pinguim-africano', especie: 'Spheniscus demersus', pais: 'Costa Sul da África', quantidade: 50000},
-    {nome:'Peixe-boi-marinho', especie: 'Trichecus manatus Linnaeus', pais: 'Brasil', quantidade: 500},
-    {nome:'Gorila-da-montanha', especie: 'Gorilla beringei beringei', pais: 'África Central', quantidade: 1000},
-    {nome:'Condor-californiano', especie: 'Gymnogyps californianus', pais: 'América do Norte', quantidade: 5000},
-    {nome:'Baleia-azul', especie: 'Balaenoptera musculus', pais: 'Antártida', quantidade: 8500}
+    { nome: 'Onça-pintada',       especie: 'Panthera onca',              pais: 'Brasil',              quantidade: 10000 },
+    { nome: 'Lobo-guará',         especie: 'Chrysocyon brachyurus',      pais: 'Brasil',              quantidade: 50    },
+    { nome: 'Panda-gigante',      especie: 'Ailuropoda melanoleuca',     pais: 'China',               quantidade: 2500  },
+    { nome: 'Baleia-fin',         especie: 'Balaenoptera physalus',      pais: 'Antártida',           quantidade: 2000  },
+    { nome: 'Arara-azul-de-lear', especie: 'Anodorhynchus leari',        pais: 'Brasil',              quantidade: 1200  },
+    { nome: 'Pinguim-africano',   especie: 'Spheniscus demersus',        pais: 'Costa Sul da África', quantidade: 50000 },
+    { nome: 'Peixe-boi-marinho',  especie: 'Trichecus manatus Linnaeus', pais: 'Brasil',              quantidade: 500   },
+    { nome: 'Gorila-da-montanha', especie: 'Gorilla beringei beringei',  pais: 'África Central',      quantidade: 1000  },
+    { nome: 'Condor-californiano',especie: 'Gymnogyps californianus',    pais: 'América do Norte',    quantidade: 5000  },
+    { nome: 'Baleia-azul',        especie: 'Balaenoptera musculus',      pais: 'Antártida',           quantidade: 8500  }
 ];
 
 let headerRow = document.createElement('tr');
@@ -23,14 +23,20 @@ headers.forEach(heText => {
     header.appendChild(content);
     headerRow.appendChild(header);
 });
-let qtdTtotal = 0;
+
 table.appendChild(headerRow);
+
+let qtdTtotal = 0;
+
 animals.forEach(animal => {
     let row = document.createElement('tr');
-
+    qtdTtotal += animal.quantidade;
+    
     Object.values(animal).forEach(text => {
         let cell = document.createElement('td');
         let cellContent = document.createTextNode(text);
+        cell.style.border = "2px solid black";
+        cell.style.padding = "0.5rem";
         cell.appendChild(cellContent);
         row.appendChild(cell);
     })
@@ -38,16 +44,21 @@ animals.forEach(animal => {
     table.appendChild(row);
 });
 
-animals.forEach(animal => {
-    qtdTtotal+= animal.quantidade;
-})
-let textfoot = 'Total de animais: '+ qtdTtotal;
+
+let textfoot = 'Total de animais: ' + qtdTtotal;
 let footer = document.createElement('td');
 let footCotent = document.createTextNode(textfoot);
+
 footer.appendChild(footCotent);
 table.appendChild(footer);
-
 main.appendChild(table)
+
+table.style.border = "2px solid black";
+table.style.padding = "0.5rem";
+main.style.display = "flex";
+main.style.justifyContent = "center";
+main.style.alignItems = "center";
+main.style.padding = "10px";
 
 
 
